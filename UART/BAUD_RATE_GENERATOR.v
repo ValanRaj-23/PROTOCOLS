aa7857160da9c605bc45d6c4be3bfe1561d4c065
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-02-27 22:54:37 +0530

    BAUD_RATE_GENERATOR.v

--- UART/BAUD_RATE_GENERATOR.v
+++ UART/BAUD_RATE_GENERATOR.v
@@ -1,41 +1,76 @@
-module baud_rate(input clk,
-		 input reset,
-		 input [1:0]baud_sel,
-		output reg baud_clk);
-	integer baud_div, count; 
+module baud_generator(
+	input clk,  
+	input reset, //reset
+	input [1:0]baud_sel, //selecting the baud
+	output intx,inrx);
 
-parameter 	baud2400 = 0,
-		baud4800 = 1,
-		baud9600 = 2,
-		baud19200 =3;
+reg bclk_rx,bclk_tx;
+
+	integer baud_div_tx = 0;
+    integer baud_div_rx = 0;
+    integer count_tx = 0;
+	integer count_rx = 0;
+	
+	parameter [2:0]	b2400 = 0,
+				b4800 = 1,
+				b9600 = 2,
+				b19200 = 3;
 
 always@(*)
 begin
 	case(baud_sel)
-		2'b00 : baud_div = 15'd28033;
-		2'b01 : baud_div = 14'd10416;
-		2'b10 : baud_div = 13'd5208;
-		2'b11 : baud_div = 12'd2604;
+		2'b00 : baud_div_tx = 15'd28033;
+		2'b01 : baud_div_tx = 14'd10416;
+		2'b10 : baud_div_tx = 13'd5208;
+		2'b11 : baud_div_tx = 12'd2604;
+	endcase
+end
+always@(*)
+begin
+	case(baud_sel)
+		2'b00 : baud_div_rx = 15'd28033;
+		2'b01 : baud_div_rx = 15'd10416;
+		2'b10 : baud_div_rx = 15'd5208 ;
+		2'b11 : baud_div_rx = 15'd2604 ;
 	endcase
 end
 
+	always@(posedge clk, negedge reset)
+	begin
+		if(!reset)
+		begin
+			bclk_tx <= 0;
+			count_tx <= 0;
+		end 
+		else if (count_tx == baud_div_tx)
+			 	begin
+			 		count_tx <=0;
+			 		bclk_tx<= ~bclk_tx;
+			 	end 
+		else
+			 		count_tx <=count_tx+1'b1;
+			  
+	end 
 
+	always@(posedge clk, negedge reset)
+	begin
+		if(!reset)
+		begin
+			bclk_rx <= 0;
+			count_rx <= 0;
+		end 
+		else if (count_rx  == baud_div_rx)
+			 	begin
+			 		count_rx <=0;
+			 		bclk_rx<= ~ bclk_rx;
+			 	end 
+		else
+			 		count_rx <=count_rx +1'b1;
+			  
+	end 
 
-always@(posedge clk, negedge reset)
-begin
-	if(!reset)
-	begin
-		baud_clk <= 0;
-		count <= 0;
-	end
-	else if(baud_clk == count)
-	begin
-		baud_clk <= ~baud_clk;
-		count <= 0;
-	end
-	else 
-	begin
-		count <= count + 1'b1;
-	end
-end
+  
+ assign intx = bclk_tx;
+ assign inrx = bclk_rx;
+	
 endmodule
